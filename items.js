52d25aa5ff0939b9eb6e313fb8e76a05ae87e40f
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-01-30 18:32:35 -0500

    added arrange data to run my tests

---
+++ items.js
@@ -0,0 +1,100 @@
+
+const weapons = [
+  {
+    name: 'longsword',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 0,
+  },
+
+  {
+    name: 'kunai',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 7,
+  },
+  
+  {
+    name: 'kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 15,
+  },
+
+  {
+    name: 'kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'PRI',
+  },
+
+  {
+    name: 'kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'DUO',
+  },
+
+  {
+    name: 'kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'TRI',
+  },
+
+  {
+    name: 'kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'TET',
+  },
+
+  {
+    name: 'kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'PEN',
+  },
+
+  {
+    name: '[+1]longsword',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 1,
+  },
+  
+  {
+    name: '[+8]kunai',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 8,
+  }
+]
+
+const armours = [
+  {
+    name: 'japanese',
+    type: 'armour',
+    durability: 100,
+    enhancement: 0,
+  },
+  
+  {
+    name: 'roman',
+    type: 'armour',
+    durability: 100,
+    enhancement: 5,
+  },
+  
+  {
+    name: 'egyptian',
+    type: 'armour',
+    durability: 100,
+    enhancement: 0,
+  },
+]
+
+module.exports = {
+  weapons,
+  armours
+}
