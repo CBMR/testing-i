ed6b811dc55040079b10339123466ca7adc51c65
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-01-31 22:19:57 -0500

    removed the variables I created and just used the names of the keys

--- enhancer.js
+++ enhancer.js
@@ -1,49 +1,46 @@
 const enhancedLevels = ['PRI', 'DUO', 'TRI', 'TET', 'PEN']
 
 const success = (successItem) => {
-  let successEnhancement = successItem.enhancement;
-  let successName = successItem.name;
-  if (successEnhancement < 15) {
-    successEnhancement++
-  } else if (successEnhancement === 15) {
-    successEnhancement = 'PRI'
+  let {enhancement, name} = successItem;
+  if (enhancement < 15) {
+    enhancement++
+  } else if (enhancement === 15) {
+    enhancement = 'PRI'
   } else {
-    const indexOfEnhanced = enhancedLevels.indexOf(successEnhancement);
-    successEnhancement = enhancedLevels[indexOfEnhanced + 1]
+    const indexOfEnhanced = enhancedLevels.indexOf(enhancement);
+    enhancement = enhancedLevels[indexOfEnhanced + 1]
   }
 
-  successName = successEnhancement <= 15 ? `[+${successEnhancement}]` + successName : `[${successEnhancement}]` + successName;
+  name = enhancement <= 15 ? `[+${enhancement}]` + name : `[${enhancement}]` + name;
 
   return {
     ...successItem,
-    enhancement: successEnhancement,
-    name: successName
+    enhancement: enhancement,
+    name: name
   }
 }
 
 const fail = (failItem) => {
-  let failEnhancement = failItem.enhancement;
-  let failName = failItem.name;
-  let failDurability = failItem.durability;
+  let {enhancement, name, durability} = failItem;
 
-  if(enhancedLevels.includes(failEnhancement)) {
-    const indexOfCurrentEnhancement = enhancedLevels.indexOf(failEnhancement)
-    failEnhancement === 'PRI' ? failEnhancement = 15 : failEnhancement = enhancedLevels[indexOfCurrentEnhancement - 1]
+  if(enhancedLevels.includes(enhancement)) {
+    const indexOfCurrentEnhancement = enhancedLevels.indexOf(enhancement)
+    enhancement === 'PRI' ? enhancement = 15 : enhancement = enhancedLevels[indexOfCurrentEnhancement - 1]
   }
 
-  if(failEnhancement < 15) {
-    failDurability = failDurability - 5;
+  if(enhancement < 15) {
+    durability =durability - 5;
   } else {
-    failDurability = failDurability - 10;
+    durability =durability - 10;
   };
 
-  failName = failEnhancement <= 15 ? `[+${failEnhancement}]` + failName : `[${failEnhancement}]` + failName;
+  name = enhancement <= 15 ? `[+${enhancement}]` + name : `[${enhancement}]` + name;
 
   return {
     ...failItem,
-    durability: failDurability,
-    enhancement: failEnhancement,
-    name: failName
+    durability:durability,
+    enhancement: enhancement,
+    name: name
   } ;
 }
 module.exports = {
