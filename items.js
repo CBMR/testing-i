4f7f681e23fb8563b427ce33a179af1c31cb8a4c
Abdiel Fernandez <fernandezabdiel@gmail.com>
2019-01-31 21:46:30 -0500

    made changes to the items list to allow easier testing

--- items.js
+++ items.js
@@ -1,5 +1,6 @@
 
 const weapons = [
+  //test cases for weapons
   {
     name: 'longsword',
     type: 'weapon',
@@ -56,6 +57,7 @@ const weapons = [
     enhancement: 'PEN',
   },
 
+  //test cases for the name
   {
     name: '[+1]longsword',
     type: 'weapon',
@@ -68,29 +70,99 @@ const weapons = [
     type: 'weapon',
     durability: 100,
     enhancement: 8,
-  }
+  },
+  {
+    name: '[PRI]kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'PRI',
+  },
+
+  {
+    name: '[DUO]kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'DUO',
+  },
+
+  {
+    name: '[TRI]kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'TRI',
+  },
+
+  {
+    name: '[TET]kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'TET',
+  },
+
+  {
+    name: '[PEN]kama',
+    type: 'weapon',
+    durability: 100,
+    enhancement: 'PEN',
+  },
 ]
 
 const armours = [
+  //expects
   {
     name: 'japanese',
     type: 'armour',
     durability: 100,
-    enhancement: 0,
+    enhancement: 9,
   },
   
   {
     name: 'roman',
     type: 'armour',
     durability: 100,
-    enhancement: 5,
+    enhancement: 15,
   },
   
   {
     name: 'egyptian',
     type: 'armour',
     durability: 100,
-    enhancement: 0,
+    enhancement: 'TRI',
+  },
+  
+  {
+    name: 'nordic',
+    type: 'armour',
+    durability: 20,
+    enhancement: 7,
+  },
+
+  {
+    name: 'chinese',
+    type: 'armour',
+    durability: 5,
+    enhancement: 9,
+  },
+
+  //toEqual
+  {
+    name: '[+9]japanese',
+    type: 'armour',
+    durability: 95,
+    enhancement: 9,
+  },
+  
+  {
+    name: '[+15]roman',
+    type: 'armour',
+    durability: 90,
+    enhancement: 15,
+  },
+  {
+    name: '[DUO]egyptian',
+    type: 'armour',
+    durability: 90,
+    enhancement: 'DUO',
   },
 ]
 
